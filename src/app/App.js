47b189450ad3ddee0aa3c0c5d80a33f240203522
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-14 04:56:55 -0500

    Add Loader.jsx and styles

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Loader from './../components/loader/Loader';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <Loader />
     </div>
   );
 }
